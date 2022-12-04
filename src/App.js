0a88223b2b023aed62ec36791d8879dadd7c5063
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-12-04 13:41:07 -0800

    Last Changes on app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react'
 import Gallery from './Components/Gallery.jsx'
 import ButtonBar from './Components/ButtonBar.jsx'
 
 function App() {
-  let [artId, setArtId] = useState (5980)
-  let [data, setData] = useState ({})
+  let [artId, setArtId] = useState(190735)
+  let [data, setData] = useState({})
+  let [userInput, setUserInput] = useState("")
 
   useEffect(() => {
-
-    console.log ("useEffect runs")
+    
+    console.log("useEffect runs")
     document.title = "Welcome to ArtWorld"
-    fetch (`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
-.then ( response => response.json())
-.then ( resData => setData(resData))
-}, [artId])
+
+    getArtData(artId)
+
+  }, [artId])
+
+  const getArtData = (id) => {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }
+
   const handleIterate = e => {
     setArtId(artId + Number(e.target.value))
   }
+
+  const handleChange = e => {
+    setUserInput(e.target.value)
+  }
+
   return (
     <div className="App">
-      <Gallery data = {data}/>
-      <ButtonBar handleIterate = {handleIterate}/>
+      <Gallery data={data}/>
+      <div className="user-input">
+        <input type="text" onChange={handleChange} placeholder="Enter art id"/>
+        <button onClick={() => getArtData(userInput)}>Submit</button>
+      </div>
+      <ButtonBar handleIterate={handleIterate}/>
     </div>
   );
 }
 
-
 export default App;
-
